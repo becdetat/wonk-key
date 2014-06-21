898f0218317ddb9394826577c7b3d4c06533dc83
Ben Scott <ben.scott@readify.net>
2014-06-21 22:07:24 +1000

    Bootstrap a macro recorder app

---
+++ src/MacroRecorder/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroRecorder
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<string> Keystrokes { get; private set; }
+
+        public MainViewModel()
+        {
+            Keystrokes = new ObservableCollection<string>(new []
+            {
+                "a","b","c"
+            });
+        }
+    }
+}
